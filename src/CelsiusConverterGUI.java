8e117ba1723dd91dcac44d7bc7fbfa33c1e6fa9a
kobanya <nagy.bela.budapest@gmail.com>
2022-10-13 22:53:54 +0200

    gui

--- src/CelsiusConverterGUI.java
+++ src/CelsiusConverterGUI.java
@@ -1,2 +1,36 @@
-public class CelsiusConverterGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CelsiusConverterGUI extends  JFrame {
+    private JPanel mainPanel;
+    private JTextField celsiusTexfield;
+    private JButton ConverterButton;
+    private JLabel FahrenheitLabel;
+
+    public CelsiusConverterGUI(String title)   {
+        super (title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        ConverterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // olvasd be az értéket
+                //alakítsd át tizedesre
+                // váltsd át
+                int tempFahr = (int)(Double.parseDouble(celsiusTexfield.getText())
+                        * 1.8 + 32);
+                FahrenheitLabel.setText(tempFahr +"  Fahremheit  ");
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame frame = new CelsiusConverterGUI("Celsius konverter");
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
