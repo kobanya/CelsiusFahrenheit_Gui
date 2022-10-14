43555026b13ac80781db34a4da38836dbddeefd2
kobanya <nagy.bela.budapest@gmail.com>
2022-10-14 06:33:15 +0200

    a forráskód megtisztítása

--- src/CelsiusConverterGUI.java
+++ src/CelsiusConverterGUI.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CelsiusConverterGUI extends  JFrame {
     private JPanel mainPanel;
@@ -15,37 +13,27 @@ public class CelsiusConverterGUI extends  JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
-        ConverterButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // olvasd be az értéket
-                //alakítsd át tizedesre
-                // váltsd át
-                int tempFahr = (int)(Double.parseDouble(celsiusTexfield.getText())
-                        * 1.8 + 32);
-                FahrenheitLabel.setText("Átváltás után a hőfok : "+tempFahr +"  Fahremheit  ");
-            }
+        ConverterButton.addActionListener(e -> {
+            // olvasd be az értéket
+            //alakítsd át tizedesre
+            // váltsd át
+            int tempFahr = (int)(Double.parseDouble(celsiusTexfield.getText())
+                    * 1.8 + 32);
+            FahrenheitLabel.setText("Átváltás után a hőfok : "+tempFahr +"  Fahremheit  ");
         });
 
-        CelsiusButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //olvasd be Fahrenheitben
-                // alakítsd át tizedes számmá
-                //váltsd át
-                int tempcelsius=(int) (Double.parseDouble(celsiusTexfield.getText())
-                      -32  / 1.8000 );
-        FahrenheitLabel.setText("Átváltás után a hőfok: "+ tempcelsius+" Celsius fok");
-            }
+        CelsiusButton.addActionListener(e -> {
+            //olvasd be Fahrenheitben
+            // alakítsd át tizedes számmá
+            //váltsd át
+            int tempcelsius=(int) (Double.parseDouble(celsiusTexfield.getText())
+                  -32  / 1.8000 );
+    FahrenheitLabel.setText("Átváltás után a hőfok: "+ tempcelsius+" Celsius fok");
         });
-        buttonClear.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // nullazd ki a beviteli mezőt
-                int tempcelsius = 0;
-                FahrenheitLabel.setText("0 Celsius/ Fahreinheit fok");
-                celsiusTexfield.setText("");
-            }
+        buttonClear.addActionListener(e -> {
+            // nullazd ki a beviteli mezőt
+            FahrenheitLabel.setText("0 Celsius/ Fahreinheit fok");
+            celsiusTexfield.setText("");
         });
     }
     public static void main(String[] args) {
@@ -53,7 +41,5 @@ public class CelsiusConverterGUI extends  JFrame {
         frame.setVisible(true);
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+
 }
